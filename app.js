0290e515526ab616b46aa7c0b4deb057527732ac
Christopher Melby <melby.christopher@gmail.com>
2020-01-15 22:23:15 -0800

    add functions reading and writing to database tables. Tomorrow finish the view functions and display the table in console for ux

--- app.js
+++ app.js
@@ -23,169 +23,173 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err
     console.log("Connected as Id" + connection.threadId)
-    // startPrompt();
+    startPrompt();
 });
 
 //================== Initial Prompt =======================//
-// function startPrompt() {
-//     inquirer.prompt([
-//     {
-//     type: "list",
-//     message: "What would you like to do?",
-//     name: "choice",
-//     choices: ["Add Employee?",
-//               "Add Role?",
-//               "Add Department?",
-//               "View All Employees?", 
-//               "View All Roles?",
-//               "View all Deparments", 
-//               "Update Employee"
-//             ]
-//     }
-// ]).then(function(val) {
-//         if (val.choices === "Add Employee?") {
-//             addEmployee();
-//         } else if (val.choices === "Add Role?") {
-//             addRole();
-//         } else if (val.choices === "Add Department?") {
-//             addDepartment();
-//         } else if (val.choices === "View All Employees?") {
-//             viewAllEmployees();
-//         } else if (val.choices === "View All Roles?") {
-//             viewAllRoles();
-//         } else if (val.choices === "View all Deparments") {
-//             viewAllDepartments();
-//         } else if (val.choices === "Update Employee") {
-//             updateEmployee();
-//         }
-//         // switch (answer.choice) {
-//         //     case "Add Employee?":
-//         //         addEmployee();
-//         //       break;
+function startPrompt() {
+    inquirer.prompt([
+    {
+    type: "list",
+    message: "What would you like to do?",
+    name: "choice",
+    choices: ["Add Employee?",
+              "Add Role?",
+              "Add Department?",
+              "View All Employees?", 
+              "View All Roles?",
+              "View all Deparments", 
+              "Update Employee"
+            ]
+    }
+]).then(function(val) {
+        switch (val.choice) {
+            case "Add Employee?":
+                addEmployee();
+              break;
       
-//         //     case "Add Role?":
-//         //         addRole();
-//         //       break;
+            case "Add Role?":
+                addRole();
+              break;
       
-//         //     case "Add Department?":
-//         //         addDepartment();
-//         //       break;
+            case "Add Department?":
+                addDepartment();
+              break;
       
-//         //     case "View All Employees?":
-//         //         viewAllEmployees();
-//         //       break;
+            case "View All Employees?":
+                viewAllEmployees();
+              break;
       
-//         //     case "View All Roles?":
-//         //         viewAllRoles();
-//         //       break;
-//         //     case "View all Deparments":
-//         //         viewAllDepartments();
-//         //       break;
-//         //     case "Update Employee":
-//         //         updateEmployee();
-//         //       break;
-//         //     }
-//     })
-// }
-addEmployee();
+            case "View All Roles?":
+                viewAllRoles();
+              break;
+            case "View all Deparments":
+                viewAllDepartments();
+              break;
+            case "Update Employee":
+                updateEmployee();
+              break;
+            }
+    })
+}
+
+// addEmployee();
 //============= Add Employee ==========================//
 function addEmployee() { 
+  connection.query("SELECT * FROM employee", function(err, results) {
+    if (err) throw err;
     inquirer.prompt([
         {
-          name: "first name",
+          name: "firstname",
           type: "input",
           message: "Enter their first name "
         },
         {
-          name: "last name",
+          name: "lastname",
           type: "input",
           message: "Enter their last name "
         },
         {
           name: "role",
           type: "list",
-          message: "What is their role? "
+          message: "What is their role? ",
+          choices: [
+                    "Sales Lead",
+                    "Salesperson",
+                    "Lead Engineer",
+                    "Software Egineer",
+                    "Account Manager",
+                    "Accountant",
+                    "Leagal Team Lead"
+          ]
         },
         {
-          name: "manager",
-          type: "list",
-          message: "Who is thier manager?",
-        }
-    ]).then(function(answer) {
+            name: "choice",
+            type: "rawlist",
+            choices: function() {
+              var choiceArray = [];
+              for (var i = 0; i < results.length; i++) {
+                choiceArray.push(results[i].manager_id);
+              }
+              return choiceArray;
+            },
+            message: "Whats their managers name?"
+          }
+    ]).then(function(val) {
         var query = connection.query(
             "INSERT INTO employee SET ?",
             {
-              first_name: answer.item,
-              last_name: answer.item,
-              role: answer.item,
-              manager: answer.item
+              first_name: val.firstname,
+              last_name: val.lastname,
+              role: val.role,
+              choices: val.choice
             },
             function(err) {
                 if (err) throw err
                 console.log("You just added an employee");
                 startPrompt();
             }
+        )   
+    });
+  });
+}
+
+//============= Add Employee Role ==========================//
+function addRole() { 
+    inquirer.prompt([
+        {
+          name: "title",
+          type: "input",
+          message: "What is the roles Title?"
+        },
+        {
+          name: "salary",
+          type: "input",
+          message: "What is the Salary?"
+
+        } 
+    ]).then(function(answer) {
+        var query = connection.query(
+            "INSERT INTO role SET ?",
+            {
+              title: answer.title,
+              salary: answer.salary,
+            },
+            function(err) {
+                if (err) throw err
+                console.log("You just added a role");
+                startPrompt();
+            }
         )
-        
+
     });
  
   }
-//============= Add Employee By Role ==========================//
-// function addRole() { 
-//     inquirer.prompt([
-//         {
-//           name: "title",
-//           type: "input",
-//           message: "What is the roles Title?"
-//         },
-//         {
-//           name: "salary",
-//           type: "input",
-//           message: "What is the Salary?"
-
-//         } 
-//     ]).then(function(answer) {
-//         var query = connection.query(
-//             "INSERT INTO role SET ?",
-//             {
-//               title: answer.item,
-//               salary: answer.item,
-//             },
-//             function(err) {
-//                 if (err) throw err
-//                 console.log("You just added a role");
-//                 startPrompt();
-//             }
-//         )
-
-//     });
- 
-//   }
 //============= Add Department ==========================//
-// function addDepartment() { 
-//     inquirer.prompt([
-//         {
-//           name: "name",
-//           type: "input",
-//           message: "What Department would you like to add?"
-//         }
-//     ]).then(function(answer) {
-//         var query = connection.query(
-//             "INSERT INTO department SET ?",
-//             {
-//               name: answer.item
+function addDepartment() { 
+    inquirer.prompt([
+        {
+          name: "name",
+          type: "input",
+          message: "What Department would you like to add?"
+        }
+    ]).then(function(answer) {
+        var query = connection.query(
+            "INSERT INTO department SET ?",
+            {
+              name: answer.name
             
-//             },
-//             function(err) {
-//                 if (err) throw err
-//                 console.log("You just added a department");
-//                 startPrompt();
-//             }
-//         )
+            },
+            function(err) {
+                if (err) throw err
+                console.log("You just added a department");
+                startPrompt();
+            }
+        )
   
-//     });
+    });
  
-//   }
+  }
 //============= View All Employees ==========================//
     // function viewAllEmployees(){
     //     readEmployeeItems().then(
@@ -250,26 +254,26 @@ function addEmployee() {
 
 //================ Read Items =================================//
 //Employee Table...........
-function readEmployeeItems(cb) {
-    connection.query("SELECT * FROM employee ", function(err, res) {
-        if (err) throw err;
-        cb(res);
-    })
-}
+// function readEmployeeItems(cb) {
+//     connection.query("SELECT * FROM employee ", function(err, res) {
+//         if (err) throw err;
+//         cb(res);
+//     })
+// }
 //Role table.............
-function readRoleItems(cb) {
-    connection.query("SELECT * FROM role ", function(err, res) {
-        if (err) throw err;
-        cb(res);
-    })
-}
+// function readRoleItems(cb) {
+//     connection.query("SELECT * FROM role ", function(err, res) {
+//         if (err) throw err;
+//         cb(res);
+//     })
+// }
 //Role table.............
-function readDepartments(cb) {
-    connection.query("SELECT * FROM department ", function(err, res) {
-        if (err) throw err;
-        cb(res);
-    })
-}
+// function readDepartments(cb) {
+//     connection.query("SELECT * FROM department ", function(err, res) {
+//         if (err) throw err;
+//         cb(res);
+//     })
+// }
 
 
 
@@ -314,3 +318,32 @@ function readDepartments(cb) {
 //   console.log(departmentTable);
 
 
+//         // switch (answer.choice) {
+//         //     case "Add Employee?":
+//         //         addEmployee();
+//         //       break;
+      
+//         //     case "Add Role?":
+//         //         addRole();
+//         //       break;
+      
+//         //     case "Add Department?":
+//         //         addDepartment();
+//         //       break;
+      
+//         //     case "View All Employees?":
+//         //         viewAllEmployees();
+//         //       break;
+      
+//         //     case "View All Roles?":
+//         //         viewAllRoles();
+//         //       break;
+//         //     case "View all Deparments":
+//         //         viewAllDepartments();
+//         //       break;
+//         //     case "Update Employee":
+//         //         updateEmployee();
+//         //       break;
+//         //     }
+//     })
+// }
